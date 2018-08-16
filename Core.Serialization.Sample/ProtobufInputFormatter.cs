99e2976f26156ed6de22b16d78d7af6bbb825655
dejanstojanovic <d.stojanovic@hotmail.com>
2018-08-16 15:20:24 +0400

    Initila commit

---
+++ Core.Serialization.Sample/ProtobufInputFormatter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Core.Serialization.Sample
+{
+    public class ProtobufInputFormatter : InputFormatter
+    {
+        public ProtobufInputFormatter()
+        {
+
+            this.SupportedMediaTypes.Clear();
+
+            this.SupportedMediaTypes.Add(MediaTypeHeaderValue.Parse("application/x-protobuf"));
+            //this.SupportedMediaTypes.Add(MediaTypeHeaderValue.Parse("application/x-google-protobuf"));
+
+        }
+
+        public override bool CanRead(InputFormatterContext context)
+        {
+            //return base.CanRead(context);
+            return true;
+        }
+
+        public override Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context)
+        {
+            
+            var inputData = context.HttpContext.Request.Body;
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Core.Serialization.Sample/ProtobufOutputFormatter.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace Core.Serialization.Sample
+{
+    public class ProtobufOutputFormatter : OutputFormatter
+    {
+        public ProtobufOutputFormatter()
+        {
+            this.SupportedMediaTypes.Clear();
+
+            this.SupportedMediaTypes.Add(MediaTypeHeaderValue.Parse("application/x-protobuf"));
+            //this.SupportedMediaTypes.Add(MediaTypeHeaderValue.Parse("application/x-google-protobuf"));
+
+
+            
+        }
+
+        public override bool CanWriteResult(OutputFormatterCanWriteContext context)
+        {
+            return true;
+            //return base.CanWriteResult(context);
+        }
+
+        public override Task WriteResponseBodyAsync(OutputFormatterWriteContext context)
+        {
+            var resultObject = context.Object;
+            byte[] serializedData= new byte[] { };
+            var outputStream = new MemoryStream();
+            context.HttpContext.Response.Body = outputStream;
+
+            return outputStream.WriteAsync(serializedData, 0, serializedData.Length);
+        }
+    }
+}
